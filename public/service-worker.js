72507d19fcedcc820e8658d262f3ec075834dee3
Rivas2302 <139265627+Rivas2302@users.noreply.github.com>
2026-04-10 12:03:15 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,13 @@
 const CACHE_NAME = 'vaultstock-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
+  e.waitUntil(clients.claim());
 });
